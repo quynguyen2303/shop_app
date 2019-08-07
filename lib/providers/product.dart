e887dc6104dee1e4e1387b554fa909f47b6d142e
23editorcs <23editorcs@gmail.com>
2019-08-07 12:47:30 +0700

    Update product.dart

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -16,4 +16,9 @@ class Product with ChangeNotifier {
       @required this.imageUrl, 
       this.isFavorite = false
       });
+
+  void toogleFavorite() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
