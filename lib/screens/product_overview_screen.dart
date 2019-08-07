c8355f75e3210e43f936d56d2196502f8a4ef1b2
23editorcs <23editorcs@gmail.com>
2019-08-07 12:47:28 +0700

    Update product_overview_screen.dart

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
-import 'package:provider/provider.dart';
-import '../providers/product.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (context) => Product(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('My Shop'),
-        ),
-        body: ProductsGrid(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
       ),
+      body: ProductsGrid(),
     );
   }
 }
+
