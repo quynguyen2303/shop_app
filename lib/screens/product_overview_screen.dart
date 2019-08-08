2ab4a5f016c1ec0b1fcc0aed5eed9c2d7f70de45
23editorcs <23editorcs@gmail.com>
2019-08-08 11:20:26 +0700

    Update product_overview_screen.dart

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions { Favorites, All }
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  bool _showFavorite = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text('Favorite'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showFavorite = true;
+                } else {
+                  _showFavorite = false;
+                }
+                print(_showFavorite);
+              });
+            },
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showFavorite),
     );
   }
 }
-
