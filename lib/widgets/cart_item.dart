f9592dacfd47c4915330e8c4f05eb0e343a6e46f
23editorcs <23editorcs@gmail.com>
2019-08-09 11:43:41 +0700

    Create cart_item.dart

---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final String productId;
+  final double price;
+  final int quantity;
+  final String title;
+
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          size: 40.0,
+          color: Colors.white,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15.0,
+          vertical: 8.0,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        // margin: EdgeInsets.all(10.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: FittedBox(
+                child: Text('$price'),
+              ),
+            ),
+          ),
+          title: Text('$title'),
+          subtitle: Text('\$${(price * quantity)}'),
+          trailing: Text('$quantity x'),
+        ),
+      ),
+    );
+  }
+}
