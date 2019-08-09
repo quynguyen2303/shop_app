81291bf3b79c4a009b6ed2779ddf13d1e32a5c0c
23editorcs <23editorcs@gmail.com>
2019-08-09 11:43:37 +0700

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../screens/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String imageUrl;
@@ -13,6 +14,8 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cartData = Provider.of<Cart>(context, listen: false);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: GridTile(
@@ -47,7 +50,9 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             color: Theme.of(context).accentColor,
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              cartData.addCartItem(product.id, product.price, product.title);
+            },
           ),
         ),
       ),
