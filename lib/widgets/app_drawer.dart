23e1143ef7582acfae7082be3e54b7f60ef12b3d
23editorcs <23editorcs@gmail.com>
2019-08-10 11:17:02 +0700

    Create app_drawer.dart

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../screens/order_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Hello, buddy!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Order'),
+            onTap: () {
+              Navigator.of(context).pushNamed(OrderScreen.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
