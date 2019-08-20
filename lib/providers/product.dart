cd0f8dafce8b22fde75101ae2a30ff5f624330d3
23editorcs <23editorcs@gmail.com>
2019-08-20 14:39:12 +0700

    Update product.dart

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/foundation.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/http_exception.dart';
+
 class Product with ChangeNotifier {
   final String id;
   final String title;
@@ -8,17 +13,23 @@ class Product with ChangeNotifier {
   final String imageUrl;
   bool isFavorite;
 
-  Product({
-      @required this.id, 
-      @required this.title, 
-      @required this.description, 
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
       @required this.price,
-      @required this.imageUrl, 
-      this.isFavorite = false
-      });
+      @required this.imageUrl,
+      this.isFavorite = false});
 
-  void toogleFavorite() {
+  Future<void> toogleFavorite() async {
+    final url = 'https://flutter-shop-28335.firebaseio.com/products/$id.json';
     isFavorite = !isFavorite;
     notifyListeners();
+    final response = await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+    if (response.statusCode >= 400) {
+      isFavorite = !isFavorite;
+      notifyListeners();
+      throw HttpException('Cannot add favorite product.');
+    }
   }
 }
