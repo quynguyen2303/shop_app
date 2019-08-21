78e94b37788294f3a28592794bdf1d66f1951a1d
23editorcs <23editorcs@gmail.com>
2019-08-21 11:28:38 +0700

    Update order.dart

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'cart.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class OrderItem {
   final String id;
@@ -22,16 +24,62 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(double amount, List<CartItem> products) {
-    // implement add new order
-    _orders.insert(
-        0,
-        OrderItem(
-          id: DateTime.now().toString(),
-          amount: amount,
-          products: products,
-          date: DateTime.now(),
-        ));
-    notifyListeners();
+  Future<void> fetchAndSetOrders() async {
+    const url = 'https://flutter-shop-28335.firebaseio.com/orders.json';
+    List<OrderItem> loadedOrders = [];
+    final response = await http.get(url);
+    final extractedOrders = json.decode(response.body) as Map<String, dynamic>;
+    // print(response.body);
+    extractedOrders.forEach((id, orderItem) {
+      final newOrderItem = OrderItem(
+        id: id,
+        amount: orderItem['amount'],
+        date: DateTime.now(),
+        products: orderItem['products']
+            .map((prod) => CartItem(
+                  id: prod['id'],
+                  title: prod['title'],
+                  price: prod['price'],
+                  quantity: prod['quantity'],
+                ))
+            .toList(),
+      );
+      loadedOrders.add(newOrderItem);
+      _orders = loadedOrders;
+    });
+  }
+
+  Future<void> addOrder(double amount, List<CartItem> products) async {
+    const url = 'https://flutter-shop-28335.firebaseio.com/orders.json';
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'amount': amount,
+            'products': products
+                .map((product) => {
+                      'id': product.id,
+                      'title': product.title,
+                      'quantity': product.quantity,
+                      'price': product.price,
+                      'total': product.price * product.quantity,
+                    })
+                .toList(),
+            'date': DateTime.now().toString(),
+          }));
+
+      // implement add new order
+      _orders.insert(
+          0,
+          OrderItem(
+            id: json.decode(response.body)['name'],
+            amount: amount,
+            products: products,
+            date: DateTime.now(),
+          ));
+      notifyListeners();
+    } catch (error) {
+      print('Failed Order');
+      throw error;
+    }
   }
 }
