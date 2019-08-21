4ba009e53eb84f6c13eb494004e00e624e31e153
23editorcs <23editorcs@gmail.com>
2019-08-21 11:28:42 +0700

    Update cart.dart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -35,7 +35,7 @@ class Cart with ChangeNotifier {
       _items.putIfAbsent(
         productId,
         () => CartItem(
-          id: DateTime.now().toString(),
+          id: productId,
           title: title,
           price: price,
           quantity: 1,
