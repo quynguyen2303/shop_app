a6474523c0c27742ef7107bae20fea2ca5305a8b
23editorcs <23editorcs@gmail.com>
2019-08-22 16:36:46 +0700

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,15 +1,50 @@
-
 import 'package:flutter/widgets.dart';
+import 'package:dio/dio.dart';
+import 'package:provider/provider.dart';
 
 class Auth with ChangeNotifier {
   String _token;
-  String _expiryDate;
+  DateTime _expiryDate;
   String _userId;
+  final apiKey = 'AIzaSyAzQi4EXcqp0GuKfXL7g1a48LygDb0xPfU';
 
-  Future<void> signup() {
-    const apiKey = 'AIzaSyAzQi4EXcqp0GuKfXL7g1a48LygDb0xPfU';
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=$apiKey';
+  bool isAuth() {
+    return token != null;
+  }
 
-    
+  String get token {
+    if (_expiryDate != null && _expiryDate.isAfter(DateTime.now()) && _token != null) {
+      return _token;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey';
+    Dio dio = Dio();
+    try {
+      final response = await dio.post(url, queryParameters: {
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      });
+      _token = response.data['idToken'];
+      _userId = response.data['email'];
+      _expiryDate = DateTime.now().add(Duration(seconds: int.parse(response.data['expiresIn'])));
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
