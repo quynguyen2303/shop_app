fc2e2f35631121b01fb53287fc09e16ac2d1d981
23editorcs <23editorcs@gmail.com>
2019-08-23 10:19:52 +0700

    Update user_product_screen.dart

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -11,36 +11,56 @@ import '../screens/user_edit_screen.dart';
 class UserProductScreen extends StatelessWidget {
   static final routeName = '/user_product';
 
-  Future<void> _refesh(BuildContext context) async {
-    await Provider.of<Products>(context).fetchAndSetProducts();
+  Future<void> _refeshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false)
+        .fetchAndSetProducts(true);
   }
+
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
-
+    // final productsData = Provider.of<Products>(context); // cannot use because infinite loop
+    // with FutureBuilder
+    print('building...');
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Your Product'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                Navigator.of(context).pushNamed(UserEditScreen.routeName);
-              },
-            )
-          ],
-        ),
-        drawer: AppDrawer(),
-        body: RefreshIndicator(
-          onRefresh: () => _refesh(context),
-          child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (context, int) => UserProductItem(
-              productsData.items[int].id,
-              productsData.items[int].title,
-              productsData.items[int].imageUrl,
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Your Product'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(UserEditScreen.routeName);
+            },
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+          future: _refeshProducts(context),
+          builder: (context, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (dataSnapshot.error != null) {
+                // Handling errors later
+                return AlertDialog();
+              } else {
+                // print('Pass connection');
+                return RefreshIndicator(
+                  onRefresh: () => _refeshProducts(context),
+                  child: Consumer<Products>(
+                    builder: (context, productsData, _ ) => ListView.builder(
+                      itemCount: productsData.items.length,
+                      itemBuilder: (context, int) => UserProductItem(
+                        productsData.items[int].id,
+                        productsData.items[int].title,
+                        productsData.items[int].imageUrl,
+                      ),
+                    ),
+                  ),
+                );
+              }
+            }
+          }),
+    );
   }
 }
