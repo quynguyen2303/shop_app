ec618e90e9a8299fd25e762444e3142ce4c70d7c
23editorcs <23editorcs@gmail.com>
2019-08-23 10:57:19 +0700

    Update order.dart

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'cart.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'dart:convert';
 
 class OrderItem {
@@ -21,69 +22,107 @@ class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
 
   final String tokenId;
+  final String userId;
+  Dio dio = Dio();
 
-  Orders(this.tokenId, this._orders);
+  Orders(this.tokenId, this.userId, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> fetchAndSetOrders() async {
-    final url = 'https://flutter-shop-28335.firebaseio.com/orders.json?auth=$tokenId';
+    final url =
+        'https://flutter-shop-28335.firebaseio.com/orders/$userId.json?auth=$tokenId';
     List<OrderItem> loadedOrders = [];
-    final response = await http.get(url);
-    final extractedOrders = json.decode(response.body) as Map<String, dynamic>;
-    // print(response.body);
-    extractedOrders.forEach((id, orderItem) {
-      loadedOrders.add(
-        OrderItem(
-          id: id,
-          amount: orderItem['amount'],
-          date: DateTime.parse(orderItem['date']),
-          products: (orderItem['products'] as List<dynamic>)
-              .map((cartItem) => CartItem(
-                    id: cartItem['id'],
-                    title: cartItem['title'],
-                    price: cartItem['price'],
-                    quantity: cartItem['quantity'],
-                  ))
-              .toList(),
-        ),
+
+    try {
+      final response = await dio.get(url);
+      // print(response.body);
+      if (response.data == null) {
+        return;
+      }
+      response.data.forEach(
+        (id, orderItem) {
+          loadedOrders.add(
+            OrderItem(
+              id: id,
+              amount: orderItem['amount'],
+              date: DateTime.parse(orderItem['date']),
+              products: (orderItem['products'] as List<dynamic>)
+                  .map((cartItem) => CartItem(
+                        id: cartItem['id'],
+                        title: cartItem['title'],
+                        price: cartItem['price'],
+                        quantity: cartItem['quantity'],
+                      ))
+                  .toList(),
+            ),
+          );
+        },
       );
-      _orders = loadedOrders;
-    });
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        print(e.response.data);
+        print(e.response.headers);
+        print(e.response.request);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        print(e.request);
+        print(e.message);
+      }
+    }
+    _orders = loadedOrders;
   }
 
   Future<void> addOrder(double amount, List<CartItem> products) async {
-    const url = 'https://flutter-shop-28335.firebaseio.com/orders.json';
+    final url =
+        'https://flutter-shop-28335.firebaseio.com/orders/$userId.json?auth=$tokenId';
     final timestamp = DateTime.now();
+
     try {
-      final response = await http.post(url,
-          body: json.encode({
-            'amount': amount,
-            'products': products
-                .map((cartItem) => {
-                      'id': cartItem.id,
-                      'title': cartItem.title,
-                      'quantity': cartItem.quantity,
-                      'price': cartItem.price,
-                      // 'total': product.price * product.quantity,
-                    })
-                .toList(),
-            'date': timestamp.toIso8601String(),
-          }));
+      final response = await dio.post(
+        url,
+        data: {
+          'creatorId': userId,
+          'amount': amount,
+          'products': products
+              .map((cartItem) => {
+                    'id': cartItem.id,
+                    'title': cartItem.title,
+                    'quantity': cartItem.quantity,
+                    'price': cartItem.price,
+                    // 'total': product.price * product.quantity,
+                  })
+              .toList(),
+          'date': timestamp.toIso8601String(),
+        },
+      );
       // implement add new order
       _orders.insert(
-          0,
-          OrderItem(
-            id: json.decode(response.body)['name'],
-            amount: amount,
-            products: products,
-            date: timestamp,
-          ));
+        0,
+        OrderItem(
+          id: response.data['name'],
+          amount: amount,
+          products: products,
+          date: timestamp,
+        ),
+      );
       notifyListeners();
-    } catch (error) {
-      throw error;
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        print(e.response.data);
+        print(e.response.headers);
+        print(e.response.request);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        print(e.request);
+        print(e.message);
+      }
     }
   }
 }
